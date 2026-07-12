fb1caaaeaa61053aa37b349bf4000318d4d7ae44
chandanKumar811-ops <dk6387811@gmail.com>
2026-07-12 07:38:39 +0530

    Create pyq.js

---
+++ pyq.js
@@ -0,0 +1,83 @@
+// Easy-NEET NEET PYQ Question Bank
+
+window.pyqQuestions = [
+
+    {
+        question: "Which cell organelle is known as the powerhouse of the cell?",
+        options: [
+            "Nucleus",
+            "Mitochondria",
+            "Ribosome",
+            "Lysosome"
+        ],
+        answer: 2,
+        subject: "Biology",
+        year: 2024
+    },
+
+    {
+        question: "The functional unit of kidney is:",
+        options: [
+            "Neuron",
+            "Nephron",
+            "Alveoli",
+            "Villi"
+        ],
+        answer: 2,
+        subject: "Biology",
+        year: 2023
+    },
+
+    {
+        question: "The SI unit of electric resistance is:",
+        options: [
+            "Volt",
+            "Ampere",
+            "Ohm",
+            "Watt"
+        ],
+        answer: 3,
+        subject: "Physics",
+        year: 2022
+    },
+
+    {
+        question: "The value of acceleration due to gravity on Earth is approximately:",
+        options: [
+            "5.8 m/s²",
+            "9.8 m/s²",
+            "12 m/s²",
+            "15 m/s²"
+        ],
+        answer: 2,
+        subject: "Physics",
+        year: 2021
+    },
+
+    {
+        question: "The chemical formula of sulphuric acid is:",
+        options: [
+            "HCl",
+            "HNO3",
+            "H2SO4",
+            "NaOH"
+        ],
+        answer: 3,
+        subject: "Chemistry",
+        year: 2020
+    },
+
+    {
+        question: "The atomic number of carbon is:",
+        options: [
+            "4",
+            "6",
+            "8",
+            "12"
+        ],
+        answer: 2,
+        subject: "Chemistry",
+        year: 2019
+    }
+
+];
