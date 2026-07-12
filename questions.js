750fe6be4f0d8ec6d22e742baef19ef1b2f7451e
chandanKumar811-ops <dk6387811@gmail.com>
2026-07-12 06:40:16 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,41 @@
+// Easy-NEET Question Bank
+
+const questionBank = [
+
+    {
+        question: "The powerhouse of the cell is:",
+        options: [
+            "Nucleus",
+            "Mitochondria",
+            "Ribosome",
+            "Golgi apparatus"
+        ],
+        answer: 2,
+        subject: "Biology"
+    },
+
+    {
+        question: "The SI unit of force is:",
+        options: [
+            "Joule",
+            "Newton",
+            "Watt",
+            "Pascal"
+        ],
+        answer: 2,
+        subject: "Physics"
+    },
+
+    {
+        question: "The chemical formula of water is:",
+        options: [
+            "CO2",
+            "H2O",
+            "O2",
+            "NaCl"
+        ],
+        answer: 2,
+        subject: "Chemistry"
+    }
+
+];
